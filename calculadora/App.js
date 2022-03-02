56b55bbb8bc37bf13df6329f54e8cef53ca6e19a
FeProg-18 <79095518+felipe-programador-18@users.noreply.github.com>
2022-03-02 10:43:33 -0300

    c1- making buttons and div View

--- calculadora/App.js
+++ calculadora/App.js
@@ -4,12 +4,16 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text> Display</Text>
-      <Text>Result</Text>
+      <Text style={styles.display} > Display</Text>
+      <Text style={styles.result}>Result</Text>
 
-      <View style={styles.button}>
-      
-      
+      <View style={styles.buttons}>
+         <View style={styles.col1}>
+        
+         </View>
+         <View style={styles.col2}>
+         
+         </View>
       </View>
     
     </View>
@@ -20,7 +24,30 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
+    alignItems: 'stretch',
     justifyContent: 'center',
   },
+  display:{
+   flex:1,
+   backgroundColor:'#EFEFEF',
+   fontSize:70,
+   textAlign:'right',
+   paddingTop:30,
+   paddingRight:10
+  },
+
+  result:{
+   flex:0.4,
+   backgroundColor:'#F87676',
+   fontSize:40,
+   textAlign: 'right',
+   paddingBottom:10,
+   paddingRight:10
+  },
+  
+  buttons:{
+  flex:5,
+  flexDirection:'row'
+  }
+
 });
