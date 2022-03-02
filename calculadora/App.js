a6670c629b8c98b1699721582195004b68962204
FeProg-18 <79095518+felipe-programador-18@users.noreply.github.com>
2022-03-02 11:09:46 -0300

    c1- create all structure of calculator

--- calculadora/App.js
+++ calculadora/App.js
@@ -2,6 +2,13 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  const Col1buttons = [
+    ['7', '8', '9'],
+    ['4','5','6'],
+    ['3', '2', '1'],
+    [',', '0', '=']
+  ]
+  
   return (
     <View style={styles.container}>
       <Text style={styles.display} > Display</Text>
@@ -9,10 +16,17 @@ export default function App() {
 
       <View style={styles.buttons}>
          <View style={styles.col1}>
-        
+          {Col1buttons.map(line => <View style={styles.line}>
+            <Text>col1</Text>
+            <Text>col1</Text>
+            <Text>col1</Text>
+            </View>
+            )}
+
          </View>
+
          <View style={styles.col2}>
-         
+           <Text>col2</Text>
          </View>
       </View>
     
@@ -48,6 +62,22 @@ const styles = StyleSheet.create({
   buttons:{
   flex:5,
   flexDirection:'row'
-  }
+  },
+  
+  line:{
+   flex:1,
+   flexDirection:'row'
+  },
+  btn:{
 
+  },
+
+  col1:{
+    flex:3,
+    backgroundColor:'grey'
+  },
+  col2:{
+    flex:1,
+    backgroundColor:'#1EF4A2'
+  }
 });
