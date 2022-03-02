797a4af3892219ecd1f72ec38472543e32aa6393
FeProg-18 <79095518+felipe-programador-18@users.noreply.github.com>
2022-03-02 10:22:30 -0300

    c0- started new project calc

---
+++ calculadora/App.js
@@ -0,0 +1,26 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text> Display</Text>
+      <Text>Result</Text>
+
+      <View style={styles.button}>
+      
+      
+      </View>
+    
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
